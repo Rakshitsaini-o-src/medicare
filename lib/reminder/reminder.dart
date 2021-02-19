533e5eff5e7ae00078a431d6405480108ce5adcb
Rakshitsaini-o-src <rakshit.53-cse-18@mietjammu.in>
2021-02-19 19:09:19 +0530

    Added Reminder completely

--- lib/reminder/reminder.dart
+++ lib/reminder/reminder.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-
-
+import 'global_bloc.dart';
+import 'homepage.dart';
+import 'package:provider/provider.dart';
 
 class MedicineReminder extends StatefulWidget {
   @override
@@ -8,8 +9,25 @@ class MedicineReminder extends StatefulWidget {
 }
 
 class _MedicineReminderState extends State<MedicineReminder> {
+  GlobalBloc globalBloc;
+
+  void initState() {
+    globalBloc = GlobalBloc();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Provider<GlobalBloc>.value(
+      value: globalBloc,
+      child: MaterialApp(
+        theme: ThemeData(
+          primarySwatch: Colors.green,
+          brightness: Brightness.light,
+        ),
+        home: HomePage(),
+        debugShowCheckedModeBanner: false,
+      ),
+    );
   }
 }
